f77492ee78ff0bb9c44c790b504b01a3b595a287
kazz <kazzzz1001@gmail.com>
2020-06-08 17:09:43 +0900

    茶埋め

--- brown_diff/3.py
+++
@@ -1,5 +0,0 @@
-import math
-
-i = int(input())
-n, m = map(int, input().split(" "))
-li = list(map(int, input().split(" ")))
---
+++ brown_diff/abc126_c.py
@@ -0,0 +1,6 @@
+import math
+
+n, k = map(int, input().split(" "))
+
+for i in range(n):
+    print(math.log2(i))
