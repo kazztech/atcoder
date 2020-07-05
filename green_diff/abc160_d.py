598ed4f53ce3089ae403de390c44b7664f6a13a6
kazz <kazzzz1001@gmail.com>
2020-07-05 20:44:32 +0900

    埋め

--- green_diff/abc160_d.py
+++ green_diff/abc160_d.py
@@ -1,10 +1,36 @@
 import math
 
-n, x, y = map(int, input().split(" "))
+n, x, y = list(map(int, input().split(" ")))
 
-ans = [0 for _ in range(n)]
+li2 = [[0 for _ in range(n)] for _ in range(n)]
+
+for s in range(n - 1):
+    for e in range(s + 1, n):
+        li2[e][s] = min(abs(e - s),
+                        abs(y - s) + 1 + abs(e - x),
+                        abs(x - s) + 1 + abs(e - y))
+
+li3 = [0 for _ in range(n + 1)]
+for mli in li2:
+    for i in mli:
+        li3[i] += 1
+
 for i in range(1, n):
-    for j in range(i + 1, n + 1):
-        ans[min(j - i, abs(i - x) + abs(j - y) + 1)] += 1
+    print(li3[i])
 
-print(*ans[1:])
+# distL = [0 for _ in range(2001)]
+# for s in range(1, n + 1):
+#     for e in range(s + 1, n + 1):
+#         _min = 0
+#         if s <= x and y <= e:
+#             _min = min(e - s, (e - y) + (x - s) + 1)
+#         elif s > x and y <= e:
+#             _min = min(s - x + 1, e - s)
+#         elif s <= x and y > e:
+#             _min = min(e - s, (x - s) + (y - e) + 1)
+#         else:
+#             _min = min((s - x) + (y - e) + 1, e - s)
+#         distL[_min] += 1
+
+# for i in range(1, n):
+#     print(distL[i])
