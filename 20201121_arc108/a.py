74017ec5f5c9ac5eded62719ae13db5efeb4f947
kazz <kazzzz1001@gmail.com>
2020-11-22 20:46:16 +0900

    arc108参加

---
+++ 20201121_arc108/a.py
@@ -0,0 +1,25 @@
+import math
+import collections
+
+
+def prime_factorize(n):
+    a = []
+    while n % 2 == 0:
+        a.append(2)
+        n //= 2
+    f = 3
+    while f * f <= n:
+        if n % f == 0:
+            a.append(f)
+            n //= f
+        else:
+            f += 2
+    if n != 1:
+        a.append(n)
+    return a
+
+
+s, p = map(int, input().split(" "))
+
+dq = sorted(prime_factorize(p))
+print(dq)
---
+++ 20201121_arc108/b.py
@@ -0,0 +1,14 @@
+import math
+
+n = int(input())
+s = input()
+
+i = 0
+while True:
+    if i >= len(s) - 2:
+        i = 0
+        if f: break
+        f = True
+    if True:
+        f = False
+    i += 1
